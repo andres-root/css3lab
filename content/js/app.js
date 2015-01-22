0b9af0935a6c05fdc099caabc6ffc7df1a76845c
andres-root <andres.root.coder@gmail.com>
2015-01-22 11:11:32 -0500

    adding angular routes and dependencies

--- content/js/app.js
+++ content/js/app.js
@@ -1,1 +1,15 @@
-app.js
+var animationApp = angular.module('animationApp', ['ngRoute', 'animationControllers']);
+
+animationApp.configure(
+	function ($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'templates/list.html',
+			controller: 'listController'
+		}).when('/:name', {
+			templateUrl: function(params) { return 'templates/' + params.name; },
+			controller: 'labController'
+		}).otherwise({
+			redirectTo: '/'
+		});
+	}
+);
